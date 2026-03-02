482d1ded487ab59e74d25c8397cedb841e408a70
rubensmorais-stack <rubens.morais@gmail.com>
2026-03-02 15:28:32 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "ShareControl"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
