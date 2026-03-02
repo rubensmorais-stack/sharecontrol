02994d83758149213fe43e802117bf45fb12d4f4
rubensmorais-stack <rubens.morais@gmail.com>
2026-03-02 15:27:16 -0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>ShareControl</h1>
+      <p>Sistema de controle de market share.</p>
+      <p>Deploy inicial funcionando 🚀</p>
+    </main>
+  );
+}
